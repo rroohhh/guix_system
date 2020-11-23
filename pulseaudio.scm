f832035c7767e38e934ffb2bbaa337e6e88474e4
Robin Ole Heinemann <robin.ole.heinemann@t-online.de>
2020-11-23 18:30:19 +0100

    add missing pulseaudio file

---
+++ pulseaudio.scm
@@ -0,0 +1,32 @@
+(define-module (pulseaudio)
+  #:use-module (guix gexp)
+  #:use-module (gnu services)
+  #:use-module (gnu services dbus)
+  #:use-module (gnu services shepherd)
+  #:use-module (gnu packages pulseaudio))
+
+
+(define-public (pulseaudio-shepherd-service _)
+  "Return a shepherd service for pulseaudio"
+  (list (shepherd-service
+         (documentation "Run pulseaudio")
+         (provision '(pulseaudio))
+         (requirement
+          `(user-processes dbus-system loopback))
+         (start #~(make-forkexec-constructor
+                   (list (string-append #$pulseaudio
+                                        "/bin/pulseaudio") "--system")))
+         (stop #~(make-kill-destructor)))))
+
+
+(define-public pulseaudio-service-type
+  (let ((pulseaudio-package (lambda (_) (list pulseaudio))))
+    (service-type (name 'pulseaudio)
+                  (extensions
+                   (list (service-extension shepherd-root-service-type
+                                            pulseaudio-shepherd-service)
+                         (service-extension dbus-root-service-type
+                                            pulseaudio-package)))
+                  (default-value '())
+                  (description
+                   "Run systemwide pulseaudio"))))
